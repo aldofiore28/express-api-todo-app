60c780cc25afd2b1404135a84d3e81f92c4e35ee
Aldo Fiore <aldo.fiore@lovehoney.co.uk>
2019-12-20 15:17:23 +0000

    lol

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env'});
 
-export const PORT: number = parseInt(process.env.PORT) || 3030;
+export const PORT: number = parseInt(process.env.PORT) || 3210;
 export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === 'production';
 export const MONGODB_URI = prod ? process.env['MONGODB_URI'] : process.env['MONGODB_URI_LOCAL'];
