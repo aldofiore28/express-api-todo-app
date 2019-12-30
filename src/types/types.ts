9751f84d9f06c966bbfe4a07cb45a5da109947a3
Aldo Fiore <aldo.fiore@lovehoney.co.uk>
2019-12-30 09:50:33 +0000

    Formatting

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,12 +1,12 @@
-import {ObjectId} from 'mongodb';
+import { ObjectId } from 'mongodb';
 
 export interface JsonResponse {
 	success: boolean;
 	message: string;
 	data: TodoInterface[] | [];
-};
+}
 
 export interface TodoInterface {
 	_id: ObjectId;
 	description: string;
-};
+}
